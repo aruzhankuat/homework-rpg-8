776fa62ff2af612e209c694af97c2e0edc7fba9f
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-04-29 10:14:52 +0500

    2

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -8,10 +8,7 @@ package com.narxoz.rpg;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO (student): Create at least 2 heroes with different starting states
-        // TODO (student): Create a sequence of ≥ 4 floors using ≥ 3 distinct floor subclasses
-        // TODO (student): Instantiate a tower runner and execute the tower climb
-        // TODO (student): Track and print results (floors cleared, heroes surviving, tower status)
-        // TODO (student): Demonstrate visible state transitions in the output
+        //bgvvg gv
+        int A = 5;
     }
 }
